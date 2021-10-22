1b784f7021a71f8fe6221d06856e07b89ba0545b
jiaheheee <leejiahe666@gmail.com>
2021-10-22 06:59:48 +0000

    updated math.c

---
+++ math.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    printf("John each hand has three apples\nDavid gave him 10 more apples\nHow many apples does John have now?\n");
+    printf("answer: (2*3)+10=");
+    int a,b,c,d;
+    a=2;
+    b=3;
+    c=10;
+    d=a*b+c;
+    printf("%d\n",d);
+}
