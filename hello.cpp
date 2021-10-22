58b1b9f8971fef7805961dd3214434a757e403b1
jiaheheee <leejiahe666@gmail.com>
2021-10-22 09:32:55 +0000

    update mathfloat.c

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,14 @@
-#include<iostream>
-using namespace std;
-int main(void){
-    cout << " hello world test \n ";
-} 
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    system("color 5");
+    printf("question 1:\n");
+    printf("John each hand has three apples\nDavid gave him 10 more apples\nHow many apples does John have now?\n");
+    printf("answer: (2*3)+10=");
+    int a,b,c,d;
+    a=2;
+    b=3;
+    c=10;
+    d=a*b+c;
+    printf("%d\n",d);
+}
---
+++ mathfloatold.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+float main(){
+    printf("\n");
+    printf("answer: (2.5*3.3)+10=");
+    float a,b,c,d;
+    a=2.5;
+    b=3.3;
+    c=10;
+    d=a*b+c;
+    printf("%f\n",d);
+}
