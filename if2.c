958eedb155e3784d22df09016761bc16c91bc80f
jiaheheee <leejiahe666@gmail.com>
2021-10-22 16:15:21 +0000

    update if.c

--- if2.c
+++ if2.c
@@ -13,7 +13,6 @@ char main(){
         }
     c=a+b;
     printf("%f+%f=%f\n",a,b,c);
-    return 0;
     
     if ("A==Y"){
         printf("减法\n");
@@ -25,3 +24,53 @@ char main(){
     printf("%f-%f=%f\n",d,e,f);
     return 0;
 }
+/*#include<stdio.h>
+#include<stdlib.h>
+char main(){
+    float a,b,c,d,e,f;
+    char A = 'A';
+    scanf("%c",&A);
+    if ("A==0") {
+        printf("加法\n");
+        scanf("%f %f",&a,&b);
+        c=a+b;
+        printf("%f+%f=%f\n",a,b,c);
+    }
+    else {
+        printf("减法\n");
+        
+        scanf("%f %f",&d,&e);
+        }
+    f=d-e;
+    printf("%f-%f=%f\n",d,e,f);
+    return 0;
+}
+
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int x;
+    float a,b,c,d,e,f;
+    scanf("%d",&x);
+    if ("x>=0")
+    {
+        printf("加法\n");
+        scanf("%f %f",&a,&b);
+        c=a+b;
+        printf("%f+%f=%f\n",a,b,c);
+    }
+    else 
+    {
+        printf("减法\n");
+        
+        scanf("%f %f",&d,&e);
+    }
+    f=d-e;
+    printf("%f-%f=%f\n",d,e,f);
+    return 0;
+}
+
+
+
+*/
