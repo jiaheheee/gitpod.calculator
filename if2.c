51045d6ec9d7ebbfa4a56477c5ea5057fe09d94d
jiaheheee <leejiahe666@gmail.com>
2021-10-23 03:11:18 +0000

    update if

--- if2.c
+++ if2.c
@@ -1,76 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
-char main(){
-    float a,b,c,d,e,f;
-    char A;
-    scanf("%c",&A);
-    printf("%c\n",A);
-    if ("A==Z") {
-        printf("加法\n");
-        }
-    if ("A==Z"){
-        scanf("%f %f",&a,&b);
-        }
-    c=a+b;
-    printf("%f+%f=%f\n",a,b,c);
-    
-    if ("A==Y"){
-        printf("减法\n");
-        }
-    if ("A==Y"){
-        scanf("%f %f",&d,&e);
-        }
-    f=d-e;
-    printf("%f-%f=%f\n",d,e,f);
-    return 0;
-}
-/*#include<stdio.h>
-#include<stdlib.h>
-char main(){
-    float a,b,c,d,e,f;
-    char A = 'A';
-    scanf("%c",&A);
-    if ("A==0") {
-        printf("加法\n");
-        scanf("%f %f",&a,&b);
-        c=a+b;
-        printf("%f+%f=%f\n",a,b,c);
-    }
-    else {
-        printf("减法\n");
-        
-        scanf("%f %f",&d,&e);
-        }
-    f=d-e;
-    printf("%f-%f=%f\n",d,e,f);
-    return 0;
-}
-
-
-#include<stdio.h>
-#include<stdlib.h>
 int main(){
-    int x;
-    float a,b,c,d,e,f;
-    scanf("%d",&x);
-    if ("x>=0")
-    {
+    float a,b,c;
+    int A;
+    printf("这里是笨蛋计算机进阶版\n加法按0\n减法按1\n乘法按2\n除法按3\n");
+    scanf("%d",&A);
+    if (A==0) {
         printf("加法\n");
         scanf("%f %f",&a,&b);
         c=a+b;
         printf("%f+%f=%f\n",a,b,c);
+        if (c>0)
+            printf("他是正数\n");
+        if (c<0)
+            printf("他是负数\n");
+        return 0;
     }
-    else 
-    {
+    if (A==1){
         printf("减法\n");
-        
-        scanf("%f %f",&d,&e);
+        scanf("%f %f",&a,&b);
+        c=a-b;
+        printf("%f-%f=%f\n",a,b,c);
+        if (c>0)
+            printf("他是正数\n");
+        if (c<0)
+            printf("他是负数\n");
+        return 0;
     }
-    f=d-e;
-    printf("%f-%f=%f\n",d,e,f);
+    if (A==1){
+        printf("减法\n");
+        scanf("%f %f",&a,&b);
+        c=a-b;
+        printf("%f-%f=%f\n",a,b,c);
+        if (c>0)
+            printf("他是正数\n");
+        if (c<0)
+            printf("他是负数\n");
+        return 0;
+    }
+    if (A==2){
+        printf("乘法\n");
+        scanf("%f %f",&a,&b);
+        c=a*b;
+        printf("%f*%f=%f\n",a,b,c);
+        if (c>0)
+            printf("他是正数\n");
+        if (c<0)
+            printf("他是负数\n");
+        return 0;
+    }
+    if (A==3){
+        printf("除法\n");
+        scanf("%f %f",&a,&b);
+    c=a/b;
+    printf("%f/%f=%f\n",a,b,c);
+    if (c>0)
+        printf("他是正数\n");
+    if (c<0)
+        printf("他是负数\n");
     return 0;
+    }
 }
-
-
-
-*/
