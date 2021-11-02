d108318a9154cbfedd880f9f113cdecc36a8ef99
jiaheheee <leejiahe666@gmail.com>
2021-11-02 14:36:10 +0000

    update 99.c

---
+++ 99.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+int num1=1,num2=1,con1;
+int main()
+{
+    for(num1=1;num1<=9;num1++)
+    {
+        for(num2=1;num2<=9;num2++)
+        {
+            printf("%d*%d = %d   ",num1,num2,num1*num2);
+        }
+            printf("\n");
+    }
+    return 0;
+}
+
+/*#include <stdio.h>
+int num1,num2,a,b,con1,con2;
+int main()
+{
+
+        for(num2=4;num2>=1;num2--)
+            printf(" ");
+        printf(" *");
+        printf("\n");
+    
+        for(num2=3;num2>=1;num2--)
+            printf(" ");
+        for(num1=2;num1>=1;num1--)
+            printf(" *");
+        printf("\n");
+        
+        for(num2=2;num2>=1;num2--)
+            printf(" ");
+        for(num1=3;num1>=1;num1--)
+            printf(" *");
+        printf("\n");
+        for(num2=1;num2>=1;num2--)
+            printf(" ");
+        for(num1=4;num1>=1;num1--)
+            printf(" *");
+        printf("\n");
+        for(num1=5;num1>=1;num1--)
+            printf(" *");
+    
+}
+
+
+
+
+#include <stdio.h>
+int num1,num2,a,b,con1,con2;
+int main()
+{
+    a=1;
+    while(a<5){
+    for(con1=4;con1>=1;con1--)
+    {
+        for(con2=4;con2>=1;con2--)
+        {
+            for(num2=4;num2>=1;num2--)
+            printf(" ");
+        
+        }
+        for(num1=1;num1>=1;num1--)
+        printf(" *");
+        printf("\n");
+    
+    a=a+1;
+    }
+    }
+}
+
+
+
+
+
+*/
